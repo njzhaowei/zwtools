7a4175a148598ba4794aa2161891f50996c33b20
njzhaowei <yewberry@126.com>
2023-07-26 23:45:34 +0800

    update

--- apps/freenode/main.py
+++ apps/freenode/main.py
@@ -24,18 +24,21 @@ URL = 'https://freenode.me/'
 
 def usage():
     print(
-'Usage: {0} [-h|--help] [-p|--permut]\n\
+'Usage: {0} [-h|--help] [-p|--permut] [-u|--url]\n\
 Description: Search url "{1}" for v2ray subscription link. Use permutation without repetition (default) or full permutation (-p) of four digital to crack access password\
 '.format(EXE_PTH.name, URL)
     )
 
-def get_subs_url():
+def get_subs_url(url):
     subs_url = None
     reqs_url = None
     reqs_pwd = None
-    resp = requests.get(URL)
-    soup = BeautifulSoup(resp.text, features='html.parser')
-    arr = soup.find_all('a', string=re.compile(r'免费.*节点.*订阅|分享'), href=re.compile(r'https://freenode.me/a/.*'))
+    if url is None:
+        resp = requests.get(URL)
+        soup = BeautifulSoup(resp.text, features='html.parser')
+        arr = soup.find_all('a', string=re.compile(r'免费.*节点.*订阅|分享'), href=re.compile(r'https://freenode.me/a/.*'))
+    else:
+        arr = [url]
 
     pwds = []
     for i in range(1,10):
@@ -43,15 +46,14 @@ def get_subs_url():
             for k in range(1,10):
                 for h in range(1,10):
                     if NUM_TYP == 0:
-                        if i!=j and j!=k and k!=h:
+                        if len({i,j,k,h}) == 4:
                             pwds.append('%s%s%s%s'%(i,j,k,h))
                     else:
                         pwds.append('%s%s%s%s'%(i,j,k,h))
 
     print('Start...')
-
     for o in arr:
-        url = o.attrs['href']
+        url = o.attrs['href'] if hasattr(o, 'href') else o
         print('Try url: %s' % url)
         with tqdm(pwds, desc='Processing', leave=True, unit='item') as pbar:
             for idx,pwd in enumerate(pwds):
@@ -90,17 +92,20 @@ def get_subs_url():
 def main():
     try:
         try:
-            opts, args = getopt.getopt(sys.argv[1:], 'hp', ['help', 'permut'])
+            opts, args = getopt.getopt(sys.argv[1:], 'hpu:', ['help', 'permut', 'url'])
         except getopt.GetoptError as err:
             # print help information and exit:
             print(err)  # will print something like "option -a not recognized"
             usage()
             sys.exit(2)
 
+        url = None
         for o, a in opts:
             if o in ('-h', '--help'):
                 usage()
                 sys.exit()
+            elif o in ('-u', '--url'):
+                url = a
             elif o in ('-p', '--permut'):
                 global NUM_TYP
                 NUM_TYP = 1
@@ -108,7 +113,7 @@ def main():
                 assert False, 'unhandled option'
         print('Exe path: %s, Unpack size: %.2fM'%( MEI_TMP, (dirsize(MEI_TMP)/1024/1024) ))
         usage()
-        get_subs_url()
+        get_subs_url(url)
     except Exception:
         traceback.print_exc()
 
